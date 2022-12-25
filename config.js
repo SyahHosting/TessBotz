ce4c2926f7d62ada7dea5d765e915b0fb34e6c3b
SyahHosting <akunfreesyah@gmail.com>
2022-12-25 20:50:55 +0700

    tesop

---
+++ config.js
@@ -0,0 +1,46 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+
+global.ownername = 'SYAHBOTZ'
+global.Anticall = false
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+// Other
+global.owner = ['628927991935','628927991935','628927991935']
+global.premium = ['628927991935']
+global.packname = 'SYAHBOTZ'
+global.author = 'WhatsApp Bot'
+global.wlcm = []
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./src/gambar/jo.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
